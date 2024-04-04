9fa6c4c148d28673cca4f5c6498e9ed36fc08ae6
Maksym Bodnar <macsimbodnar@gmail.com>
2024-04-04 19:04:22 +0200

    Add game info and downalod pgn button

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -6,14 +6,21 @@ from pydantic import BaseModel
 import logging
 import chess
 import chess.engine
+from typing import Optional
 
 
 class MoveInput(BaseModel):
     fen: str
+    time: Optional[float]
+    depth: Optional[int]
 
 
 class MoveResult(MoveInput):
     best_move: str
+    w_score: Optional[int]
+    b_score: Optional[int]
+    pv: list[str]
+    nodes: int
 
 
 log_instance = logging.getLogger("fastapi")
@@ -25,7 +32,7 @@ def log(log: str):
 
 
 engine = chess.engine.SimpleEngine.popen_uci(
-    "/home/max/Downloads/stockfish/stockfish-ubuntu-x86-64-avx2")
+    "/home/max/Downloads/Software/stockfish-ubuntu-x86-64-avx2/stockfish/stockfish-ubuntu-x86-64-avx2")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -34,11 +41,45 @@ templates = Jinja2Templates(directory="templates")
 
 @app.post("/make_move", response_model=MoveResult)
 async def post_make_move(board_status: MoveInput):
-    board = chess.Board(board_status.fen)
-    move = engine.play(board, chess.engine.Limit(time=0.1))
-    board.push(move.move)
 
-    result = MoveResult(fen=board.fen(), best_move=str(move.move))
+    board = chess.Board(board_status.fen)
+
+    info = None
+    if board_status.time > 0:
+        info = engine.analyse(
+            board, chess.engine.Limit(time=board_status.time))
+
+    if board_status.depth > 0:
+        info = engine.analyse(
+            board, chess.engine.Limit(depth=board_status.depth))
+
+    if board_status.depth == 0 and board_status.time == 0:
+        info = engine.analyse(
+            board, chess.engine.Limit(time=0.01))
+
+    if not info:
+        raise HTTPException(
+            status_code=500, detail="Failed to execute the engine")
+
+    # PV is Principal variation
+    best_move = info['pv'][0]
+
+    board.push(best_move)
+    fen = board.fen()
+
+    pv = [str(move) for move in info['pv']]
+    score = info['score']
+
+    result = MoveResult(
+        fen=fen,
+        best_move=str(best_move),
+        w_score=score.white().score(),
+        b_score=score.black().score(),
+        pv=pv,
+        nodes=info['nodes'],
+        time=info['time'],
+        depth=info['depth'])
+
     return result
 
 
